9f945dcff7a165c53d9db543f65909c0cb47ef3c
Argenis De La Rosa <hennyrosexo@gmail.com>
2019-11-26 00:22:04 -0500

    updated the ui with dummy data

--- Craps/ViewController.swift
+++ Craps/ViewController.swift
@@ -6,15 +6,36 @@
 //  Copyright © 2019 argenis delarosa. All rights reserved.
 //
 
+
+// ALL IMAGE ASSETS WERE PROVIDED BY:
+// <div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+//
+//
+
+
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var crapsLabel: UILabel!
+    
+//    let diceArray = [, "2", "3", "4", "5", "6"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+        
+        
+    }
+    
 }
 
