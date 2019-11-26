5e71a16157e60599461680d3312488b757219dee
Argenis De La Rosa <hennyrosexo@gmail.com>
2019-11-26 01:08:03 -0500

    updated the dice conditions

--- Craps/ViewController.swift
+++ Craps/ViewController.swift
@@ -17,23 +17,45 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     @IBOutlet weak var crapsLabel: UILabel!
     
-//    let diceArray = [, "2", "3", "4", "5", "6"]
+    let dice = ["1", "2", "3", "4", "5", "6"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
-        diceImageView1.image = diceArray[Int.random(in: 0...5)]
-        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+        let leftNumber = Int(arc4random())
+        let rightNumber = Int(arc4random())
+        diceImageView1.image = UIImage(named: dice[leftNumber])
+        diceImageView2.image = UIImage(named: dice[rightNumber])
         
+        if leftNumber == 0 && rightNumber == 0 {
+            crapsLabel.text = "SNAKE EYES!"
+        }
+        else if leftNumber == 4 && rightNumber == 4 {
+            crapsLabel.text = "Hardaway Pays 7 to 1"
+        }
+        else if leftNumber == 2 && rightNumber == 2 {
+            crapsLabel.text = "Hardaway Pays 9 to 1"
+        }
+        else if leftNumber == 1 && rightNumber == 1 {
+            crapsLabel.text = "Hardaway Pays 7 to 1"
+        }
+        else if leftNumber == 0 && rightNumber == 1 {
+            crapsLabel.text = "Craps! You lost!"
+        }
+        else if leftNumber == 4 && rightNumber == 4 {
+            crapsLabel.text = "SNAKE EYES!"
+        }
+        else if leftNumber == 4 && rightNumber == 4 {
+            crapsLabel.text = "SNAKE EYES!"
+        }
         
     }
     
